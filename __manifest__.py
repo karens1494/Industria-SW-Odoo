a8a1f7d23c49ade074af32d1a2cae0af7df0a056
karens1494 <57648917+karens1494@users.noreply.github.com>
2019-11-18 22:22:19 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
+        'security/security.xml',
         'templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
