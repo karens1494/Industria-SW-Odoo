1bc41e25320efb39b15b6ea7727c55d36dd29a9b
karens1494 <57648917+karens1494@users.noreply.github.com>
2019-11-18 21:41:33 -0600

    Update models.py

--- models/models.py
+++ models/models.py
@@ -14,7 +14,20 @@ class Course(models.Model):
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
     
-        _sql_constraints = [
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+    
+    _sql_constraints = [
         ('name_description_check',
          'CHECK(name != description)',
          "The title of the course should not be the description"),
