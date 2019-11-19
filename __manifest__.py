7c2bf0ce8c72b79e3d40d1eb47b00f22814e1d7f
karens1494 <57648917+karens1494@users.noreply.github.com>
2019-11-19 14:34:00 -0600

    Update manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'report'],
+    'depends': ['base', 'report', 'board'],
 
     # always loaded
     'data': [
@@ -30,6 +30,7 @@
         'templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
         'reports.xml',
     ],
     # only loaded in demonstration mode
